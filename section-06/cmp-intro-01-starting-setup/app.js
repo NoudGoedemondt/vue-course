fbf6740ebd15748c04c30af47412cc1ed65b1965
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-04-22 20:24:05 +0200

    started section 6

---
+++ section-06/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friendList: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+});
+
+app.mount("#app");
