69c90d118be42d3f86078f31affd90c0fdd60126
Noud Goedemondt <noud.goedemondt@pti.nl>
2024-04-23 09:54:37 +0200

    finished section 6

--- section-06/cmp-intro-01-starting-setup/app.js
+++ section-06/cmp-intro-01-starting-setup/app.js
@@ -20,4 +20,35 @@ const app = Vue.createApp({
   methods: {},
 });
 
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+  },
+});
+
 app.mount("#app");
