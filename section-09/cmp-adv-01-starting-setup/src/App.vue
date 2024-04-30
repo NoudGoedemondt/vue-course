f794267a171b1dd8a0dcdfafa1e0a47a2a50c54c
Noud Goedemondt <noud.goedemondt@pti.nl>
2024-04-30 14:59:20 +0200

    finished section 9

--- section-09/cmp-adv-01-starting-setup/src/App.vue
+++ section-09/cmp-adv-01-starting-setup/src/App.vue
@@ -1,34 +1,45 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="selectComponent('active-goals')">Active goals</button>
+    <button @click="selectComponent('manage-goals')">Manage goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    UserInfo,
-    BadgeList,
+    // UserInfo,
+    // BadgeList,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    selectComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ section-09/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,28 @@
+<template>
+  <base-card>
+    <ul>
+      <li v-for="(goal, index) in goals" :key="goal">
+        <slot :goal="goal" :index="index"></slot>
+      </li>
+    </ul>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Finish course", "Master Vue"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  text-align: center;
+  list-style-type: none;
+  margin: 0;
+  padding: 0dvb;
+}
+</style>
