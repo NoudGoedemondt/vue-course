f3801d7e1052f7418090ff8a9067ecd0cb94c878
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-02 18:46:34 +0200

    started section 10

---
+++ section-10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Find information on the internet',
+          link: 'https://www.google.com/',
+        },
+        {
+          id: 'udemy',
+          title: 'Udemy',
+          description: 'Follow programming courses',
+          link: 'https://www.udemy.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
