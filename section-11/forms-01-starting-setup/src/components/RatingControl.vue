5248257c577951a9ab7403ba723d87796f4540a3
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-09 16:55:27 +0200

    finished section 11

---
+++ section-11/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li :class="{ selected: modelValue === 'poor' }">
+      <button type="button" @click="registerScore('poor')">Poor</button>
+    </li>
+    <li :class="{ selected: modelValue === 'average' }">
+      <button type="button" @click="registerScore('average')">Average</button>
+    </li>
+    <li :class="{ selected: modelValue === 'great' }">
+      <button type="button" @click="registerScore('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  // data() {
+  //   return {
+  //     score: this.modelValue,
+  //   };
+  // },
+  methods: {
+    registerScore(score) {
+      //this.score = score;
+      this.$emit('update:modelValue', score);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.selected {
+  background-color: rgba(211, 211, 211, 0.304);
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
--- section-11/forms-01-starting-setup/src/components/TheForm.vue
+++ section-11/forms-01-starting-setup/src/components/TheForm.vue
@@ -2,15 +2,23 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        :class="{ invalid: userNameValidity === 'invalid' }"
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="checkNameValidity"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name.</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="userAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,32 +27,80 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="interest"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="interest"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="interest"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          value="video"
+          v-model="how"
+        />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          value="blogs"
+          v-model="how"
+        />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          value="other"
+          v-model="how"
+        />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
+    </div>
+    <div class="form-control">
+      <input
+        type="checkbox"
+        id="confirm-terms"
+        name="confirm-terms"
+        v-model="confirm"
+      />
+      <label for="confirm-terms">Agree to terms of use</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -53,14 +109,42 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+  components: {
+    RatingControl,
+  },
   data() {
     return {
       userName: '',
+      userAge: null,
+      referrer: 'google',
+      interest: [],
+      how: null,
+      confirm: false,
+      userNameValidity: 'pending',
+      rating: null,
     };
   },
   methods: {
-    submitForm() {},
+    submitForm() {
+      console.log(`Name: ${this.userName}`);
+      console.log(`Age: ${this.userAge}`);
+      console.log(`Referrer: ${this.referrer}`);
+      console.log(this.interest);
+      console.log(this.how);
+      console.log(this.confirm);
+      console.log(this.rating);
+      this.rating = null;
+    },
+    checkNameValidity() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
+    },
   },
 };
 </script>
@@ -77,6 +161,10 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+input.invalid {
+  border-color: red;
 }
 
 label {
