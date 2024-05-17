d9c6d632091accfe9f07391cfafa1931bb5bb4a5
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-17 16:35:27 +0200

    finished 178

--- section-13/routing-04-styling-active-links/src/components/teams/TeamMembers.vue
+++ section-13/routing-04-styling-active-links/src/components/teams/TeamMembers.vue
@@ -21,6 +21,7 @@ export default {
     UserItem,
   },
   inject: ['teams', 'users'],
+  props: ['teamId'],
   data() {
     return {
       teamName: '',
@@ -28,9 +29,9 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route) {
+    loadTeamMembers(teamId) {
       // find appropriate team members via route params
-      const teamId = route.params.teamId;
+      //const teamId = route.params.teamId;
       const selectedTeam = this.teams.find((team) => team.id === teamId);
       const members = selectedTeam.members;
       const selectedMembers = [];
@@ -43,11 +44,11 @@ export default {
     },
   },
   created() {
-    this.loadTeamMembers(this.$route);
+    this.loadTeamMembers(this.teamId);
   },
   watch: {
-    $route(newRoute) {
-      this.loadTeamMembers(newRoute);
+    teamId(newId) {
+      this.loadTeamMembers(newId);
     },
   },
 };
