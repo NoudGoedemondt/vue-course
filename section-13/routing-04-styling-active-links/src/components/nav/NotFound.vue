9ba51a63f135d6df9f0286ee2597d651204e5f3c
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-19 16:35:08 +0200

    finished 181

---
+++ section-13/routing-04-styling-active-links/src/components/nav/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Page not found!</h2>
+    <router-link to="/">back to home</router-link>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
