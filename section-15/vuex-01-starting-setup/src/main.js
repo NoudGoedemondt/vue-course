219d20298c72e22eb94627ffb2b6f66a3ac5a7cc
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-31 10:34:12 +0200

    finished 219

--- section-15/vuex-01-starting-setup/src/main.js
+++ section-15/vuex-01-starting-setup/src/main.js
@@ -17,6 +17,21 @@ const store = createStore({
       state.counter = state.counter + payload.value;
     },
   },
+  getters: {
+    finalCounter(state, getters) {
+      return state.counter * 2;
+    },
+    normalizedCounter(_, getter) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
+  },
 });
 
 const app = createApp(App);
