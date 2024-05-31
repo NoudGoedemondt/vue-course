5f04467bbb1de7017ab5e9f266830bf416ff780a
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-31 13:24:20 +0200

    finished 223

---
+++ section-15/vuex-01-starting-setup/src/components/UserAuth.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ isAuth }}</p>
+  <button @click="logIn" v-if="!isAuth">Login</button>
+  <button @click="logOut" v-if="isAuth">Logout</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    ...mapActions(['logIn', 'logOut']),
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
+  },
+};
+</script>
