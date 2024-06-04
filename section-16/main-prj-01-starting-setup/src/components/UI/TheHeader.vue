ef5de0b0bd1b16d30b7b63f5c9cca8b357e87dad
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-06-04 10:57:59 +0200

    added routes and page components, committed correctly now

---
+++ section-16/main-prj-01-starting-setup/src/components/UI/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <router-link to="/coaches">coaches</router-link>
+    <router-link to="/requests">requests</router-link>
+  </header>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+a {
+  margin: 1rem;
+}
+</style>
