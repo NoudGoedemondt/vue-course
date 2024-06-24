8c9f45df8d457791e6107bf63b1a0552cdf8883a
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-06-24 12:37:35 +0200

    added coaches module

--- section-16/main-prj-01-starting-setup/src/store/index.js
+++ section-16/main-prj-01-starting-setup/src/store/index.js
@@ -1,85 +1,22 @@
 import { createStore } from 'vuex';
 import requests from './modules/requests';
+import coaches from './modules/coaches';
 
 const store = createStore({
-  modules: { requests },
+  modules: { requests, coaches },
   state() {
     return {
-      coaches: [
-        {
-          id: 'c1',
-          firstName: 'Noud',
-          lastName: 'Goedemondt',
-          areas: ['backend', 'career'],
-          description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore odio, nemo, temporibus, quae minus magnam blanditiis repellat beatae hic accusamus aperiam eum maxime ab. Ad voluptas accusantium excepturi eius fuga.',
-          rate: 49.99,
-        },
-        {
-          id: 'c2',
-          firstName: 'Max',
-          lastName: 'Schwarz',
-          areas: ['frontend', 'career'],
-          description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore odio, nemo, temporibus, quae minus magnam blanditiis repellat beatae hic accusamus aperiam eum maxime ab. Ad voluptas accusantium excepturi eius fuga.',
-          rate: 29.99,
-        },
-        {
-          id: 'c3',
-          firstName: 'Henk',
-          lastName: 'Janse',
-          areas: ['frontend', 'backend'],
-          description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore odio, nemo, temporibus, quae minus magnam blanditiis repellat beatae hic accusamus aperiam eum maxime ab. Ad voluptas accusantium excepturi eius fuga.',
-          rate: 99.99,
-        },
-        {
-          id: 'c4',
-          firstName: 'John',
-          lastName: 'Doe',
-          areas: ['frontend', 'career'],
-          description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore odio, nemo, temporibus, quae minus magnam blanditiis repellat beatae hic accusamus aperiam eum maxime ab. Ad voluptas accusantium excepturi eius fuga.',
-          rate: 19.99,
-        },
-        {
-          id: 'c5',
-          firstName: 'Jane',
-          lastName: 'Johnson',
-          areas: ['frontend'],
-          description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore odio, nemo, temporibus, quae minus magnam blanditiis repellat beatae hic accusamus aperiam eum maxime ab. Ad voluptas accusantium excepturi eius fuga.',
-          rate: 19.99,
-        },
-      ],
       userId: '',
     };
   },
   getters: {
-    hasCoaches(state) {
-      return state.coaches && state.coaches.length > 0;
-    },
-    getCoachById: (state) => (id) => {
-      return state.coaches.find((coach) => coach.id === id);
-    },
-    filteredCoaches: (state) => (selectedAreas) => {
-      return state.coaches.filter((coach) =>
-        selectedAreas.every((area) => coach.areas.includes(area))
-      );
-    },
     hasUserId(state) {
       return state.userId !== '';
     },
   },
   mutations: {
-    ADD_COACH(state, coachData) {
-      state.coaches.unshift(coachData);
-      state.userId = coachData.id;
-    },
-  },
-  actions: {
-    addCoach(context, coachData) {
-      context.commit('ADD_COACH', coachData);
+    SET_USER_ID(state, coachId) {
+      state.userId = coachId;
     },
   },
 });
