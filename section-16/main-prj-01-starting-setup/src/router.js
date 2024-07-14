cdc6fea62f39dde082570753f4a3c74d9aee6d33
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-07-14 16:11:36 +0200

    add global nav guards

--- section-16/main-prj-01-starting-setup/src/router.js
+++ section-16/main-prj-01-starting-setup/src/router.js
@@ -6,6 +6,7 @@ import UserAuth from './pages/UserAuth.vue';
 import ContactCoach from './pages/ContactCoach.vue';
 import RequestsReceived from './pages/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -17,11 +18,29 @@ const router = createRouter({
       component: ContactCoach,
       props: true,
     },
-    { path: '/register', component: CoachRegistration },
-    { path: '/login', component: UserAuth },
-    { path: '/requests', component: RequestsReceived },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    { path: '/login', component: UserAuth, meta: { requiresUnauth: true } },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
     { path: '/:notFound(.*)', component: NotFound },
   ],
+});
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/login');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
