dd4659bdf6a27e0448bdd4a8fb8eaea597d5cc59
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-07-30 12:12:21 +0200

    migrate App

--- section-19/composition-13-demo-starting-project/src/App.vue
+++ section-19/composition-13-demo-starting-project/src/App.vue
@@ -5,29 +5,17 @@
   </main>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import USER_DATA from './dummy-data.js';
-
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
 
-export default {
-  components: {
-    UserList,
-    ProjectsList,
-  },
-  data() {
-    return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
-};
+const selectedUser = ref(null);
+const activeUsers = ref(USER_DATA);
+
+const selectUser = (userId) =>
+  (selectedUser.value = activeUsers.value.find((user) => user.id === userId));
 </script>
 
 <style>
