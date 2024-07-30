18176090c378901b3392a9c10d0b46894fc25a0d
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-07-30 11:45:01 +0200

    migrate UserList, third attempt

--- section-19/composition-13-demo-starting-project/src/components/users/UserList.vue
+++ section-19/composition-13-demo-starting-project/src/components/users/UserList.vue
@@ -25,68 +25,56 @@
   </base-container>
 </template>
 
-<script>
+<script setup>
+import { defineProps, ref, computed, watch } from 'vue';
 import UserItem from './UserItem.vue';
 
-export default {
-  components: {
-    UserItem,
-  },
-  props: ['users'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-      sorting: null,
-    };
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
-    },
-  },
-  computed: {
-    availableUsers() {
-      let users = [];
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) =>
-          usr.fullName.includes(this.activeSearchTerm)
-        );
-      } else if (this.users) {
-        users = this.users;
-      }
-      return users;
-    },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers;
-      }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (this.sorting === 'asc') {
-          return -1;
-        } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    },
-  },
-};
+const props = defineProps(['users']);
+
+const enteredSearchTerm = ref('');
+const activeSearchTerm = ref('');
+const sorting = ref(null);
+
+watch(enteredSearchTerm, (value) => {
+  setTimeout(() => {
+    if (value === enteredSearchTerm.value) {
+      activeSearchTerm.value = value;
+    }
+  }, 300);
+});
+
+const availableUsers = computed(() => {
+  let users = [];
+  if (activeSearchTerm.value) {
+    users = props.users.filter((user) =>
+      user.fullName.includes(activeSearchTerm.value)
+    );
+  } else if (props.users) {
+    users = props.users;
+  }
+  return users;
+});
+
+const displayedUsers = computed(() => {
+  if (!sorting.value) {
+    return availableUsers.value;
+  }
+  return availableUsers.value.slice().sort((u1, u2) => {
+    if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+      return 1;
+    } else if (sorting.value === 'asc') {
+      return -1;
+    } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+});
+
+const updateSearch = (value) => (enteredSearchTerm.value = value);
+
+const sort = (mode) => (sorting.value = mode);
 </script>
 
 <style scoped>
